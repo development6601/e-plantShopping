5b62a99be57e609df52dada9dd54513a0cf2e41c
development6601 <development6601@gmail.com>
2026-01-03 16:18:03 +0530

    config issue resolved

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact/",
+  base: '/shoppingreact/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
